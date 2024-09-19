41cc36c27e3875ee7501353b9f6aa37228ffd674
Ajay Kumar <devilajay1090@gmail.com>
2024-09-19 10:52:25 +0530

    mobile wallet btn fixed

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -30,27 +30,43 @@ const Header = () => {
 
                         {/* menu large screen */}
                         <div className="col-7 col-lg-8 p-0 d-flex align-center justify-content-end menu-large">
-                            <ul className='d-flex m-0 p-0 nav-menu me-4'>
+                            <div className="menu d-flex align-center">
+                                <ul className='d-flex m-0 p-0 nav-menu me-4'>
+                                    {
+                                        menu.map((y, idx) => (
+                                            <Link href={y.url} key={idx} className={`${y.name === 'Home' ? 'active' : ''}`}><li>{y.name}</li></Link>
+                                        ))
+                                    }
+                                </ul>
+                                <VscColorMode className={`${theme} me-3 theme-mode`} onClick={toggleTheme} />
                                 {
-                                    menu.map((y, idx) => (
-                                        <Link href={y.url} key={idx} className={`${y.name === 'Home' ? 'active' : ''}`}><li>{y.name}</li></Link>
-                                    ))
+                                    connected ? (
+                                        <div className="rewards" id="rewards">
+                                            <FaAward className="cn-icon" onClick={() => setSoon(!soon)} />
+                                            <p className="soon rounded-pill" hidden={soon}>soon!</p>
+                                        </div>
+                                    ) : (' ')
                                 }
-                            </ul>
-                            <VscColorMode className={`${theme} me-3 theme-mode`} onClick={toggleTheme} />
-                            {
-                                connected ? (
-                                    <div className="rewards" id="rewards">
-                                        <FaAward className="cn-icon" onClick={() => setSoon(!soon)} />
-                                        <p className="soon rounded-pill" hidden={soon}>soon!</p>
-                                    </div>
-                                ) : (' ')
-                            }
+                            </div>
                             <WalletButtons />
+                            <div className="menu-mobile">
+                                <RxHamburgerMenu className='toggle-btn' onClick={() => setMobileMenu(!mobileMenu)} />
+                                <div className={`toggle-menu ${mobileMenu ? 'active' : ' '}`}>
+                                    <IoClose className="menu-close" onClick={() => setMobileMenu(!mobileMenu)} />
+                                    <ul className='m-0 p-0 nav-menu'>
+                                        {
+                                            menu.map((y, idx) => (
+                                                <Link href={y.url} key={idx} className={`${y.name === 'Home' ? 'active' : ''}`} onClick={() => setMobileMenu(!mobileMenu)}><li>{y.name}</li></Link>
+                                            ))
+                                        }
+                                    </ul>
+                                    <VscColorMode className={`${theme} me-3 theme-mode`} onClick={toggleTheme} />
+                                </div>
+                            </div>
                         </div>
 
                         {/* Mobile Menu */}
-                        <div className="col-7 col-lg-8 p-0 menu-mobile d-none">
+                        {/* <div className="col-7 col-lg-8 p-0 menu-mobile d-none">
                             <WalletButtons />
                             <RxHamburgerMenu className='toggle-btn' onClick={() => setMobileMenu(!mobileMenu)} />
                             <div className={`toggle-menu ${mobileMenu ? 'active' : ' '}`}>
@@ -64,10 +80,10 @@ const Header = () => {
                                 </ul>
                                 <VscColorMode className={`${theme} me-3 theme-mode`} onClick={toggleTheme} />
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
-            </section>
+            </section >
         </>
     )
 }
