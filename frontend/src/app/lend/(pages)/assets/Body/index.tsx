f9e876faeba91b2185f3c9586e714ace9dbb4b9f
Ajay Kumar <devilajay1090@gmail.com>
2024-09-24 13:32:53 +0530

    lend info

--- frontend/src/app/lend/(pages)/assets/Body/index.tsx
+++ frontend/src/app/lend/(pages)/assets/Body/index.tsx
@@ -8,6 +8,7 @@ import { useApp } from "@/context/AppProvider"
 import { Listing } from "@/types/ApiInterface"
 import { useWallet } from "@aptos-labs/wallet-adapter-react"
 import { IoNewspaperOutline } from "react-icons/io5";
+import { MdCollections } from "react-icons/md";
 
 export function Body() {
     const { account } = useWallet();
@@ -92,6 +93,12 @@ export function TokenListings({ viewtype, tokens, loading }: TokenListingsProps)
                                 <div className="card-body ">
                                     <h4 className="card-title">{token.token_name}</h4>
                                     <p className="card-text p-2">{token.collection_name}</p>
+                                    <h6 className="px-2">Borrower's Ask:</h6>
+                                    <p className="p-2">
+                                        <span className="txt-primary">{token.amount} {token.coin ? getAssetByType(token.coin)?.symbol : ""}</span> with
+                                        <span className="txt-primary">{token.apr}</span> APR for
+                                        <span className="txt-primary">{token.duration}</span> days.
+                                    </p>
                                     <button onClick={() => setChosenToken(token)} data-bs-toggle="modal" data-bs-target={`#${lendModalId}`} className="btn list-btn w-100 mt-3">Give Loan</button>
                                 </div>
                             </div>
