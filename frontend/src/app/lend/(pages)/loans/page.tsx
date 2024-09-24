e0ef81f45a92081604cbcfb7c7c870d56211afac
Ajay Kumar <62602303+ajaythxkur@users.noreply.github.com>
2024-09-24 17:02:04 +0530

    updated metadata

--- frontend/src/app/lend/(pages)/loans/page.tsx
+++ frontend/src/app/lend/(pages)/loans/page.tsx
@@ -1,5 +1,10 @@
 // lend/loans (Loans given)
+import { project } from "@/utils/constants"
 import { Body } from "./Body"
+export const metadata = {
+    title: `Loans - ${project}`,
+    description: "Loans Given on assets"
+}
 export default function Page(){
     return(
         <Body />
