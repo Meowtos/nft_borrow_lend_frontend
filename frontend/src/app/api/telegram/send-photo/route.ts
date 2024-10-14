05b3809b10843f5d9c0dcc059a11f3f282be2217
Ajay Kumar <62602303+ajaythxkur@users.noreply.github.com>
2024-10-14 16:54:45 +0530

    telegram integrated

---
+++ frontend/src/app/api/telegram/send-photo/route.ts
@@ -0,0 +1,32 @@
+import { TG_CHAT_ID } from "@/utils/env";
+import { sendTgMessage } from "@/utils/telegram-api";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest){
+    try {
+        const body = await req.json();
+        let caption = `🟩 🟩 🟩 🟩 🟩 🟩 🟩\n\nNew Listing\n\n<b>${body.title}</b>`
+        if(body.amount){
+            caption += `\n\nAmount - ${body.amount} ${body.coin ?? "Any Coin"}`
+        } 
+        if(body.apr){
+            caption += `\n\nAPR - ${body.apr} %`
+
+        }
+        if(body.duration){
+            caption += `\n\nLoan Duration - ${body.duration} days`
+        }
+        // caption += `Give Loan - ${body.url}` // After mainnet launch
+        caption += `\n\n<a href="https://meowtos.xyz">View Here</a>`
+        await sendTgMessage(TG_CHAT_ID, body.image, caption);
+        return NextResponse.json({ message: "success" })
+    } catch (error: unknown) {
+        console.log(error)
+        let errorMessage = 'An unexpected error occurred';
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        return NextResponse.json({ message: errorMessage }, { status: 500 });
+    }
+}
+
